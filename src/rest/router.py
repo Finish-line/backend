bc2726226b2d82e82db92a4e03c17c151995f010
Jakob Liebig <liebig04@proton.me>
2024-11-24 03:58:02 +0100

    added endpoint distance

--- src/rest/router.py
+++ src/rest/router.py
@@ -19,11 +19,11 @@ def calculate_distance(
     
 
 rides = [
-    {'id': 0, 'price': 0.05, 'duration' : 600, 'start': 'Schwabing-West', 'start_lat': 48.165980,'start_long': 11.556090, 'destination': 'Schwabing-Freimann', 'destination_lat': 48.188645, 'destination_long':11.585273},
-    {'id': 1, 'price': 0.08, 'duration' : 1560, 'start': 'Schwabing-West', 'start_lat': 48.157868,'start_long': 11.561289, 'destination': 'Bogenhausen', 'destination_lat': 48.146300, 'destination_long':11.606730},
-    {'id': 2, 'price': 0.06, 'duration' : 720, 'start': 'Schwabing-West', 'start_lat': 48.160261,'start_long': 11.551723, 'destination': 'Neuhausen-Nymphenburg', 'destination_lat': 48.148188, 'destination_long':11.508284},
-    {'id': 3, 'price': 0.04, 'duration' : 960, 'start': 'Altstadt-Lehel', 'start_lat': 48.137020,'start_long': 11.580554, 'destination': 'Feldmüllerweg', 'destination_lat': 48.113244, 'destination_long':11.585446},
-    {'id': 4, 'price': 0.03, 'duration' : 540, 'start': 'Altstadt-Lehel', 'start_lat': 48.138376,'start_long': 11.580843, 'destination': 'Sendling', 'destination_lat': 48.118267, 'destination_long':11.552381}
+    {'id': 0, 'price': 0.05, 'duration' : 600, 'start': 'Schwabing-West', 'startLat': 48.165980,'startLong': 11.556090, 'destination': 'Schwabing-Freimann', 'destLat': 48.188645, 'destLong':11.585273},
+    {'id': 1, 'price': 0.08, 'duration' : 1560, 'start': 'Schwabing-West', 'startLat': 48.157868,'startLong': 11.561289, 'destination': 'Bogenhausen', 'destLat': 48.146300, 'destLong':11.606730},
+    {'id': 2, 'price': 0.06, 'duration' : 720, 'start': 'Schwabing-West', 'startLat': 48.160261,'startLong': 11.551723, 'destination': 'Neuhausen-Nymphenburg', 'destLat': 48.148188, 'destLong':11.508284},
+    {'id': 3, 'price': 0.04, 'duration' : 960, 'start': 'Altstadt-Lehel', 'startLat': 48.137020,'startLong': 11.580554, 'destination': 'Feldmüllerweg', 'destLat': 48.113244, 'destLong':11.585446},
+    {'id': 4, 'price': 0.03, 'duration' : 540, 'start': 'Altstadt-Lehel', 'startLat': 48.138376,'startLong': 11.580843, 'destination': 'Sendling', 'destLat': 48.118267, 'destLong':11.552381}
 ]
 
 @router.get("/rides")
